26d25ce211aff766f4429ce52793e64dfabcbf2a
tpham13 <tpham0123@gmail.com>
2021-02-08 23:17:23 -0800

    commit tree 1ac91fd1bb492d839aa4cdfe83326c708ef7d068

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+function SimplerComponent(props) {
+    return (
+        <div onClick={this.props.handleClick}>
+            "I am just happy"
+        </div>
+    )
+}
+
+export default SimplerComponent;
